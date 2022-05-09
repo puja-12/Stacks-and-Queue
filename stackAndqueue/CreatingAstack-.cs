2bae45dff417e6e3a6ff36c6b2b79f58043b446d
puja <pujarana1796@gmail.com>
2022-05-09 16:51:35 +0530

    puja added-first commit

---
+++ stackAndqueue/CreatingAstack-.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stackAndqueue
+{
+    class CreatingAstack_
+    {
+        StackNode root;
+
+        public class StackNode
+        {
+            public int data;
+            public StackNode next;
+
+            public StackNode(int data) { this.data = data; }
+        }
+
+        public bool isEmpty()
+        {
+            if (root == null)
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+
+        public void push(int data)
+        {
+            StackNode newNode = new StackNode(data);
+
+            if (root == null)
+            {
+                root = newNode;
+            }
+            else
+            {
+                StackNode temp = root;
+                root = newNode;
+                newNode.next = temp;
+            }
+            Console.WriteLine(data + " pushed to stack");
+        }
+
+        public int peek()
+        {
+            if (root == null)
+            {
+                Console.WriteLine("Stack is empty");
+                return int.MinValue;
+            }
+            else
+            {
+                return root.data;
+            }
+        }
+
+    }  
+        
+}
+
--- stackAndqueue/Program.cs
+++ stackAndqueue/Program.cs
@@ -1,2 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Welcome to stack And Queue programmimg!");
+using System;
+namespace stackAndqueue
+{
+    class program
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Welcome to stack And Queue programmimg!");
+            CreatingAstack_ sll = new CreatingAstack_();
+
+            sll.push(70);
+            sll.push(30);
+            sll.push(56);
+
+            Console.WriteLine("Top element is " + sll.peek());
+        }
+    }
+}
+
+
+
+
